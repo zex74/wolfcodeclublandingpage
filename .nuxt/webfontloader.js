0fdcdb35a0dfd0a3e8502e9570103a031befe7c9
Zeljko Lazarevic <zeljko.stefany@gmail.com>
2021-04-17 11:35:48 +0200

    build

---
+++ .nuxt/webfontloader.js
@@ -0,0 +1,6 @@
+
+import WebFont from 'webfontloader'
+
+const options = {"custom":{"families":["Open Sans Condensed::ital,wght@0,300;0,700;1,300&display=swap","Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"],"urls":["https:\u002F\u002Ffonts.googleapis.com\u002Fcss2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap","https:\u002F\u002Ffonts.googleapis.com\u002Fcss2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"]}}
+
+WebFont.load(options)
